babace3f35422c12eea26d1d34784b2a1eda3fd4
Bernhard Braeu <bernhard.braeu@doka.com>
2019-06-06 14:53:27 +0200

    initial check in

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import BubbleChart from './BubbleChart';
+
+ReactDOM.render(<BubbleChart />, document.getElementById('root'));
